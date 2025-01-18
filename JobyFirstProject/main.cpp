6d6862263d39757af0c81f0abc6c6ae567239eb6
ChadLMitchell <ChadLMitchell@gmail.com>
2025-01-17 19:43:46 -0800

    Initial Commit

---
+++ JobyFirstProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JobyFirstProject
+//
+//  Created by Chad Mitchell on 1/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
